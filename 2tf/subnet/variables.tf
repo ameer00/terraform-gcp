0612a4ff624fff089525d6f9caf847823fd2fee9
Ameer Abbas <ameer00@gmail.com>
2018-06-02 11:39:40 -0700

    Update variables.tf

--- 2tf/subnet/variables.tf
+++ 2tf/subnet/variables.tf
@@ -3,3 +3,8 @@ variable "project" {}
 variable "region" {}
 variable "network" {}
 variable "ip_range" {}
+variable "pod_subnet" {}
+variable "svc1_subnet" {}
+variable "svc2_subnet" {}
+variable "svc3_subnet" {}
+variable "svc4_subnet" {}
