c3aacc8ef4845ae3b0d3b0be0e1925c7f82cac60
Ameer Abbas <ameer00@gmail.com>
2018-06-02 08:11:23 -0700

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -16,29 +16,29 @@ resource "google_compute_network" "vpc" {
 resource "google_compute_subnetwork" "subnet" {
  count              = 3
  name               = "vpc-${count.index}-nodes"
- ip_cidr_range      = "vpc-${count.index}_node_subnet}"
+ ip_cidr_range      = "${vpc-${count.index}_node_subnet}"
  network            = "vpc-${count.index}"
  depends_on         = ["google_compute_network.vpc"]
  region             = "${var.region}"
  secondary_ip_range = {
       range_name    = "vpc-${count.index}-pod"
-      ip_cidr_range = "vpc-${count.index}_pod_subnet}"
+      ip_cidr_range = "${vpc-${count.index}_pod_subnet}"
   }
  secondary_ip_range = {
       range_name    = "vpc-${count.index}-svc1"
-      ip_cidr_range = "vpc-${count.index}_svc1_subnet}"
+      ip_cidr_range = "${vpc-${count.index}_svc1_subnet}"
   }
  secondary_ip_range = {
       range_name    = "vpc-${count.index}-svc2"
-      ip_cidr_range = "vpc-${count.index}_svc2_subnet}"
+      ip_cidr_range = "${vpc-${count.index}_svc2_subnet}"
   }
  secondary_ip_range = {
       range_name    = "vpc-${count.index}-svc3"
-      ip_cidr_range = "vpc-${count.index}_svc3_subnet}"
+      ip_cidr_range = "${vpc-${count.index}_svc3_subnet}"
   }
  secondary_ip_range = {
       range_name    = "vpc-${count.index}-svc4"
-      ip_cidr_range = "vpc-${count.index}_svc4_subnet}"
+      ip_cidr_range = "${vpc-${count.index}_svc4_subnet}"
   }
 }
 
@@ -51,7 +51,7 @@ resource "google_container_cluster" "primary" {
   subnetwork              = "vpc-${count.index}-nodes"
   initial_node_count      = 3
   private_cluster         = "true"
-  master_ipv4_cidr_block  = "cluster-${count.index}_master_ip}"
+  master_ipv4_cidr_block  = "${cluster-${count.index}_master_ip}"
   ip_allocation_policy    = {
    cluster_secondary_range_name  = "vpc-${count.index}-pod"
    services_secondary_range_name = "vpc-${count.index}-svc1"
