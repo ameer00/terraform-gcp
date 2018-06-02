f26c0e5ae899ff9e88f444af64036867291f98ac
Ameer Abbas <ameer00@gmail.com>
2018-06-02 11:39:01 -0700

    Update main.tf

--- 2tf/subnet/main.tf
+++ 2tf/subnet/main.tf
@@ -6,22 +6,22 @@ resource "google_compute_subnetwork" "subnet" {
  region             = "${var.region}"
  secondary_ip_range = {
       range_name    = "${var.network}-pod"
-      ip_cidr_range = "${var.network}_subnet_pod"
+      ip_cidr_range = "${var.pod_subnet}"
   }
  secondary_ip_range = {
       range_name    = "${var.network}-svc1"
-      ip_cidr_range = "${var.network}_subnet_svc1"
+      ip_cidr_range = "${var.svc1_subnet}"
   }
  secondary_ip_range = {
       range_name    = "${var.network}-svc2"
-      ip_cidr_range = "${var.network}_subnet_svc2"
+      ip_cidr_range = "${var.svc2_subnet}"
   }
  secondary_ip_range = {
       range_name    = "${var.network}-svc3"
-      ip_cidr_range = "${var.network}_subnet_svc3"
+      ip_cidr_range = "${var.svc3_subnet}"
   }
  secondary_ip_range = {
       range_name    = "${var.network}-svc4"
-      ip_cidr_range = "${var.network}_subnet_svc4"
+      ip_cidr_range = "${var.svc4_subnet}"
   }
 }
