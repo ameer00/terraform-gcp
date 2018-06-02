9b456dc7e386a8b9ddf0e910009810a1f5ca98f0
Ameer Abbas <ameer00@gmail.com>
2018-06-02 11:58:06 -0700

    Update main.tf

--- 2tf/gke/main.tf
+++ 2tf/gke/main.tf
@@ -1,1 +1,13 @@
-w
+resource "google_container_cluster" "primary" {
+  name                    = "${var.name}"
+  zone                    = "${var.zone}"
+  network                 = "${var.network}"
+  subnetwork              = "vpc-${count.index}-nodes"
+  initial_node_count      = 3
+  private_cluster         = "true"
+  master_ipv4_cidr_block  = "cluster-${count.index}_master_ip"
+  ip_allocation_policy    = {
+   cluster_secondary_range_name  = "vpc-${count.index}-pod"
+   services_secondary_range_name = "vpc-${count.index}-svc1"
+   }
+}
