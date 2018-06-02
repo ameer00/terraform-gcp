b47e84014aca5da288472a8eb79e4ce6b0944b42
Ameer Abbas <ameer00@gmail.com>
2018-06-02 11:48:50 -0700

    Update main.tf

--- 2tf/main.tf
+++ 2tf/main.tf
@@ -7,5 +7,19 @@ module "project" {
 
 module "vpc" {
   source     = "./vpc"
-  vpc       = "vpc1000"
+  vpc        = "vpc1000"
+}
+ 
+module "subnet" {
+  source      = "./subnet"
+  name        = "subnet1"
+  project     = "${var.project}"
+  region      = "${var.region}"
+  network     = "vpc1000"
+  ip_range    = "172.16.0.0/22"
+  pod_range   = "10.0.0.0/14"
+  svc1_subnet = "192.168.0.0/24"
+  svc2_subnet = "192.168.1.0/24"
+  svc3_subnet = "192.168.2.0/24"
+  svc4_subnet = "192.168.3.0/24"
 }
