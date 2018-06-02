a834b35057c556591c260b750ce1b62b3bc410d6
Ameer Abbas <ameer00@gmail.com>
2018-06-02 07:53:38 -0700

    Update variables.tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -2,12 +2,29 @@ variable "region" {}
 variable "zone" {}
 variable "project" {}
 variable "credentials" {}
-variable "vpc1" {}
-variable "vpc1_node_subnet" {}
-variable "vpc1_pod_subnet" {}
-variable "vpc1_svc1_subnet" {}
-variable "vpc1_svc2_subnet" {}
-variable "vpc1_svc3_subnet" {}
-variable "vpc1_svc4_subnet" {}
-variable "cluster1" {}
-variable "cluster1_master_ip" {}
+
+variable "vpc_0_node_subnet" {}
+variable "vpc_0_pod_subnet" {}
+variable "vpc_0_svc1_subnet" {}
+variable "vpc_0_svc2_subnet" {}
+variable "vpc_0_svc3_subnet" {}
+variable "vpc_0_svc4_subnet" {}
+
+variable "vpc_1_node_subnet" {}
+variable "vpc_1_pod_subnet" {}
+variable "vpc_1_svc1_subnet" {}
+variable "vpc_1_svc2_subnet" {}
+variable "vpc_1_svc3_subnet" {}
+variable "vpc_1_svc4_subnet" {}
+
+variable "vpc_2_node_subnet" {}
+variable "vpc_2_pod_subnet" {}
+variable "vpc_2_svc1_subnet" {}
+variable "vpc_2_svc2_subnet" {}
+variable "vpc_2_svc3_subnet" {}
+variable "vpc_2_svc4_subnet" {}
+
+variable "cluster_0_master_ip" {}
+variable "cluster_1_master_ip" {}
+variable "cluster_2_master_ip" {}
+
