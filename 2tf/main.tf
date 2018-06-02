d97c4c230c90254d689c27452c0b62f460bd4dc7
Ameer Abbas <ameer00@gmail.com>
2018-06-02 11:29:17 -0700

    Update main.tf

--- 2tf/main.tf
+++ 2tf/main.tf
@@ -1,11 +1,11 @@
 module "project" {
- source      = "/project"
+ source      = "./project"
  credentials = "${file("${var.credentials}")}"
  project     = "${var.project}" 
  region      = "${var.region}"
 }
 
 module "vpc" {
-  source     = "/vpc"
-  name       = "vpc1000"
+  source     = "./vpc"
+  vpc       = "vpc1000"
 }
