3b7a380a9b1a09be3468a1ad6b2213d26ceee4eb
Ameer Abbas <ameer00@gmail.com>
2018-06-02 19:11:55 -0700

    Update main.tf

--- prod/project/main.tf
+++ prod/project/main.tf
@@ -1,5 +1,6 @@
 // Configure the Google Cloud provider
 provider "google" {
+ version = "~> 1.13"
  credentials = "${file("${var.credentials}")}"
  project     = "${var.project}" 
  region      = "${var.region}"
